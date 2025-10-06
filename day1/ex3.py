2d49432e9845112d4f0d64339bc771f3b45a74bb
rajkonkret <rajkonkret660@gmail.com>
2025-10-06 14:24:17 +0200

    [1 11] add ex3

---
+++ day1/ex3.py
@@ -0,0 +1,20 @@
+import openpyxl
+
+wb = openpyxl.load_workbook('video2.xlsx')
+ws = wb.active
+
+ws = wb['vgsales']
+
+row_position = 2
+col_position = 7
+
+total_sales = ((ws.cell(row=row_position, column=col_position).value)
+               + (ws.cell(row=row_position, column=col_position + 1).value)
+               + (ws.cell(row=row_position, column=col_position + 2).value)
+               + (ws.cell(row=row_position, column=col_position + 3).value)
+               )
+
+ws.cell(row=2, column=11).value = total_sales
+
+wb.save('video2.xlsx')
+wb.close()
