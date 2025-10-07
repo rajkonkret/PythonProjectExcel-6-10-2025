b36efd9e58feb7d7c0d182ecf60b4bca14bd4531
rajkonkret <rajkonkret660@gmail.com>
2025-10-07 12:34:28 +0200

    [2 21] update ex14py

--- day2/ex14.py
+++ day2/ex14.py
@@ -5,3 +5,18 @@ df = pd.DataFrame({
     "Wiek": [29, 24, 35],
     "Miasto": ['Warszawa', "Wrocław", "Legnica"]
 })
+
+# kontekst menadżer
+with pd.ExcelWriter("Tabela.xlsx", engine='xlsxwriter') as writer:
+    df.to_excel(writer, sheet_name="Osoby", startrow=1, header=False, index=False)
+
+    workbook = writer.book
+    worksheet = writer.sheets['Osoby']
+
+    header_format = workbook.add_format({
+        'bold': True,
+        'text_wrap': True,
+        'valign': 'vcenter',
+        'bg_color': '#D7E4BC',
+        'border': 1
+    })
