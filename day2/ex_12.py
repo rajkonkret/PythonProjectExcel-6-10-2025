45c62d913eeee87d2484b7394610d70d298e01c5
rajkonkret <rajkonkret660@gmail.com>
2025-10-07 12:19:14 +0200

    [2 17] update ex_12.py

--- day2/ex_12.py
+++ day2/ex_12.py
@@ -40,3 +40,36 @@ print(df)
 # 1000   Jhon    33     USA    6.7   Ameryka
 # 1002    Tim    41     USA    3.9   Ameryka
 # 1003  Jenny    12  Niemcy    0.0    Europa
+
+df.info()
+# <class 'pandas.core.frame.DataFrame'>
+# Index: 4 entries, 1001 to 1003
+# Data columns (total 5 columns):
+#  #   Column     Non-Null Count  Dtype
+# ---  ------     --------------  -----
+#  0   imie       4 non-null      object
+#  1   wiek       4 non-null      int64
+#  2   kraj       4 non-null      object
+#  3   ocena      4 non-null      float64
+#  4   kontynent  4 non-null      object
+# dtypes: float64(1), int64(1), object(3)
+# memory usage: 192.0+ bytes
+
+print(df.index)  # Index([1001, 1000, 1002, 1003], dtype='int64')
+
+df.index.name = "numer"
+print(df)
+#         imie  wiek    kraj  ocena kontynent
+# numer
+# 1001    Mark    55  Włochy    4.5    Europa
+# 1000    Jhon    33     USA    6.7   Ameryka
+# 1002     Tim    41     USA    3.9   Ameryka
+# 1003   Jenny    12  Niemcy    0.0    Europa
+
+# sortowanie danych
+print(df.sort_values(['kontynent', "wiek"]))
+# numer
+# 1000    Jhon    33     USA    6.7   Ameryka
+# 1002     Tim    41     USA    3.9   Ameryka
+# 1003   Jenny    12  Niemcy    0.0    Europa
+# 1001    Mark    55  Włochy    4.5    Europa
