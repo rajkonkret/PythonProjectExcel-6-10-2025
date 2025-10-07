56029bc24a9a1de553d99c69aca9c59213ca5987
rajkonkret <rajkonkret660@gmail.com>
2025-10-07 13:00:33 +0200

    [2 24] update ex15

---
+++ day2/ex15.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("height_file.csv")
+print(df)
+
+writer = pd.ExcelWriter("excel_from_csv.xlsx", engine="xlsxwriter")
+
+df.to_excel(writer, sheet_name="first_sheet", index=False)
+writer.close()
