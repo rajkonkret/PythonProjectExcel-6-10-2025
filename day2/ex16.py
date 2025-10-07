cea63f61f1597d904ac6fd6210c8da56a32f527b
rajkonkret <rajkonkret660@gmail.com>
2025-10-07 13:03:24 +0200

    [2 25]add ex16

---
+++ day2/ex16.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+height_data = [
+    {"Name": "Adiya", "Height": 179},
+    {"Name": "Samen", "Height": 181},
+    {"Name": "Darek", "Height": 170},
+    {"Name": "Jhon", "Height": 167},
+]
+
+weight_data = [
+    {"Name": "Adiya", "Weight": 79},
+    {"Name": "Samen", "Weight": 81},
+    {"Name": "Darek", "Weight": 70},
+    {"Name": "Jhon", "Weight": 67},
+]
+
+marks_data = [
+    {"Name": "Adiya", "Marks": 179},
+    {"Name": "Samen", "Marks": 181},
+    {"Name": "Darek", "Marks": 170},
+    {"Name": "Jhon", "Marks": 167},
+]
