903ffa43970a6713716015eed3a7e0bb683a7376
rajkonkret <rajkonkret660@gmail.com>
2025-10-07 11:31:21 +0200

    [2 12a] add ex_11

---
+++ day2/ex_11.py
@@ -0,0 +1,11 @@
+import math
+
+import numpy as np
+
+# pip install numpy
+
+# ndarray - tablice, macierze
+array = np.array([10, 100, 1000.])
+print(array)  # [  10.  100. 1000.]
+
+print(array.dtype)  # float64
